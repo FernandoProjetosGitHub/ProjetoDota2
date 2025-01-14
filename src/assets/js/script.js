324d1fdcaebf0c54d85ac4aa43011c1aaa0fb661
gabriela2003_00 <gabriel.ferreira@cecremge.org.br>
2025-01-14 20:11:02 -0300

    [docs] - Commit inicial

---
+++ src/assets/js/script.js
@@ -0,0 +1,24 @@
+
+export class Heroes{
+
+  constructor (){
+    this.heroes = [
+      {
+        "nome": "Rubic",
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png"
+        },
+        {
+        "nome": "tiny",
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png"
+        }
+    ];
+  }
+
+  allHeroes(){
+    return this.heroes;
+  }
+  
+}
+
+
+
