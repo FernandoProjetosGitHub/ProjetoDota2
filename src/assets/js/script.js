fa7523712a9de418f6a6cf7dc0b3ddd75faf1c8f
wesleyjp <wesleyjp7l@hotmail.com>
2025-01-14 22:52:22 -0300

    Criado função para requisição dos herois na API do dota2

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -14,6 +14,25 @@ export class Heroes{
     ];
   }
 
+  this.herois = function(){
+    const xhr = new XMLHttpRequest();
+    let resultado;
+    function buscarCep(valor) {
+        xhr.open('GET', `https://api.opendota.com/api/heroes`);
+        xhr.send();
+
+        xhr.onreadystatechange = () => {
+            if (xhr.status == 200) {
+              if (xhr.responseText != "") {
+                  return JSON.parse(xhr.responseText)
+                };
+            }
+        }
+      }  
+      
+    }
+  }
+
   allHeroes(){
     return this.heroes;
   }
