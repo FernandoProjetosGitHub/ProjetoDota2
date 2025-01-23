6ba0b475d14247cd01e37270a86448f1cde0caa4
FernandoProjetosGitHub <fernandoaugustoprojetos@gmail.com>
2025-01-22 23:10:10 -0300

    [html/css] Atualizando modificações no projeto

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,19 +5,23 @@ export class Heroes {
     this.heroes = [
       {
         "nome": "Rubic",
-        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png"
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png",
+        // "primary_attr": "agi" 
       },
       {
         "nome": "tiny",
-        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png"
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png",
+        // "primary_attr": "str",
       },
       {
         "nome": "Rubic",
-        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png"
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png",
+        // "primary_attr": "int"
       },
       {
         "nome": "tiny",
-        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png"
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png",
+        // "primary_attr": "all" // <div class="heroesAttribute">${heroiAtual.img}</div>
       },
       {
         "nome": "Rubic",
