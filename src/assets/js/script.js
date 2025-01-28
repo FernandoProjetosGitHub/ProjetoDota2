cbd06d71202974d723dd7afe0851568816283607
wesleyjp <wesleyjp7l@hotmail.com>
2025-01-27 21:42:13 -0300

    mudanças no codigo

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,37 +1,64 @@
 
-export class Heroes{
-
-  constructor (){
+/*export class Heroes {
+  constructor() {
     this.heroes = [
       {
         "nome": "Rubic",
+        "img": "http://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png",
+        // "primary_attr": "agi" 
+      },
+      {
+        "nome": "tiny",
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png",
+        // "primary_attr": "str",
+      },
+      {
+        "nome": "Rubic",
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png",
+        // "primary_attr": "int"
+      },
+      {
+        "nome": "tiny",
+        "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png",
+        // "primary_attr": "all" // <div class="heroesAttribute">${heroiAtual.img}</div>
+      },
+      {
+        "nome": "Rubic",
         "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/rubick.png"
-        },
-        {
+      },
+      {
         "nome": "tiny",
         "img": "https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/tiny.png"
-        }
+      }
     ];
   }
 
-  this.herois = function(){
-    const xhr = new XMLHttpRequest();
-    let resultado;
-    function buscarCep(valor) {
-        xhr.open('GET', `https://api.opendota.com/api/heroes`);
-        xhr.send();
+  allHeroes() {
+    return this.heroes;
+  }
 
-        xhr.onreadystatechange = () => {
-            if (xhr.status == 200) {
-              if (xhr.responseText != "") {
-                  return JSON.parse(xhr.responseText)
-                };
-            }
-        }
-      }  
-      
+}*/
+
+
+
+export class Heroes{
+
+  constructor (){
+    this.heroes = null;
+    this.URL = "https://api.opendota.com/api/heroes";
+  }
+  
+  async function getHeroes() {
+    const resp = await fetch(this.URL);
+    f (!resp.ok) {
+      throw new Error(`Erro na requisição: ${resp.statusText}`);
+    }
+    if (resp.status === 200) {
+      const obj= await resp.json();
+      this.heroes = obj;
     }
   }
+
 
   allHeroes(){
     return this.heroes;
